f1c32ccc51c84e449db89137477ae580304cad1d
Ali Mirza <106017617+alimirza09@users.noreply.github.com>
2025-08-25 22:04:25 +0500

    changed some stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,10 @@ alias ll="exa --icons=always -lAh"
 alias la="exa --icons=always -la"
 alias install="sudo xbps-install -S"
 alias remove="sudo xbps-remove -R"
+alias ip='ip --color=auto'
 alias gc="sudo git add . && sudo git commit -am "
 set -gx EDITOR nvim
 starship init fish | source
-
 function y
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
