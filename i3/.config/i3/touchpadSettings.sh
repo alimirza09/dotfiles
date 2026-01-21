07e474edadafaba4dd8b5e0aba6c7b9daa3ebd6c
alimirza09 <106017617+alimirza09@users.noreply.github.com>
2026-01-21 14:40:45 +0000

    fixed picom for arch

--- i3/.config/i3/touchpadSettings.sh
+++ i3/.config/i3/touchpadSettings.sh
@@ -1,2 +1,2 @@
-xinput set-prop "Elan Touchpad" "Synaptics Scrolling Distance" -73 73
-xinput set-prop "Elan Touchpad" "Synaptics Tap Action" 1 3 0 0 1 3 2
+xinput set-prop 9 318 1
+xinput set-prop 9 326 1
